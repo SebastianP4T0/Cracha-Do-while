711b78a2d27f02b6a4d5a62d648949315728ffc4
Sebastian Nolll <93086343+SebastianP4T0@users.noreply.github.com>
2021-10-24 18:13:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const userSocialMedia = {
+  github: 'SebastianP4T0',
+  youtube: 'UCq4OSUJoYM9Oe0O2Pj8Fh6w',
+  facebook: 'Sebastian Santos Noll',
+  instagram: 'sebastian_nolls',
+  twitter: 'sebastiannolls'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${userSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${userSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userProfile.href = data.html_url
+      userProfilePic.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGithubProfileInfos()
